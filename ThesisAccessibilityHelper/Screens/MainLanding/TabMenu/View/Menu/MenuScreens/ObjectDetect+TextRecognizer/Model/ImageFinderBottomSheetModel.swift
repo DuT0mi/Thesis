e0f023dce5cfbb41e665687a781bbb1bd9330477
Dudas Tamas Alex <dudas.tamas.alex@gmail.com>
2023-10-14 00:09:39 +0200

    feature: Image spanshot + UI elements

---
+++ ThesisAccessibilityHelper/Screens/MainLanding/TabMenu/View/Menu/MenuScreens/ObjectDetect+TextRecognizer/Model/ImageFinderBottomSheetModel.swift
@@ -0,0 +1,8 @@
+//
+//  ImageFinderBottomSheetModel.swift
+//  ThesisAccessibilityHelper
+//
+//  Created by Dudas Tamas Alex on 2023. 10. 13..
+//
+
+import Foundation
---
+++ ThesisAccessibilityHelper/Utils+Consts/Extensions/View+snapshot.swift
@@ -0,0 +1,8 @@
+//
+//  View+snapshit.swift
+//  ThesisAccessibilityHelper
+//
+//  Created by Dudas Tamas Alex on 2023. 10. 13..
+//
+
+import Foundation
