cdc2df4ba0af2495ca9f79b3b186077ea7769033
Dudas Tamas Alex <dudas.tamas.alex@gmail.com>
2023-10-13 20:49:55 +0200

    feature: New UI element for real time camera + haptics

---
+++ ThesisAccessibilityHelper/Common/Components/SingletonManagers/HapticManager.swift
@@ -0,0 +1,8 @@
+//
+//  HapticManager.swift
+//  ThesisAccessibilityHelper
+//
+//  Created by Dudas Tamas Alex on 2023. 10. 13..
+//
+
+import Foundation
---
+++ ThesisAccessibilityHelper/Screens/MainLanding/TabMenu/View/Menu/MenuScreens/ObjectDetect+TextRecognizer/Views/ImageFinderBottomSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageFinderBottomSheetView.swift
+//  ThesisAccessibilityHelper
+//
+//  Created by Dudas Tamas Alex on 2023. 10. 13..
+//
+
+import SwiftUI
+
+struct ImageFinderBottomSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageFinderBottomSheetView()
+}
